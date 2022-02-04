d379ab04880b56a85f17d4aa17c55e2a50f4451d
Duc Vinh Le <vinhxp95@gmail.com>
2022-02-04 18:00:53 +1100

    update file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import CarModelBody from './components/CarModelBody';
+import ComponentDidUpdateDemo from './components/componentDidUpdateDemo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CarModelBody></CarModelBody>
+      {/* <ComponentDidUpdateDemo></ComponentDidUpdateDemo> */}
     </div>
   );
 }
